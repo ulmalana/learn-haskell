2598509b3b29816eb18e9ceb6018a76f2d9c2fa9
ulmalana <muhamaul@gmail.com>
2021-11-30 21:32:14 +0700

    [Haskell] added chapter 2

---
+++ chapter-2/first.hs
@@ -0,0 +1,8 @@
+doubleMe x = x + x
+doubleUs x y = x*2 + y*2
+-- doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x = if x > 100
+ then x
+ else x*2
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+conanO'Brien = "It's me, Conan O'Brien"
