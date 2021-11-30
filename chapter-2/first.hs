788fb945238cd2d142c7ff039b64e9151127ad2f
ulmalana <muhamaul@gmail.com>
2021-11-30 21:54:59 +0700

    [Haskell] added chapter 2

--- chapter-2/first.hs
+++ chapter-2/first.hs
@@ -6,3 +6,5 @@ doubleSmallNumber x = if x > 100
  else x*2
 doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
 conanO'Brien = "It's me, Conan O'Brien"
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+length' xs = sum [1 | _ <- xs]
