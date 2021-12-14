03a3a5fb89eb35f6fbcb0e0166e59199175fc419
ulmalana <muhamaul@gmail.com>
2021-12-14 20:03:51 +0700

    [Rust] added more of chapter 8

--- chapter-8/type.hs
+++ chapter-8/type.hs
@@ -37,3 +37,71 @@ baseCircle r = Circle (Point 0 0) r
 
 baseReact :: Float -> Float -> Shape
 baseReact width height = Rectangle (Point 0 0) (Point width height)
+
+----------
+
+data Person = Person String String Int Float String String deriving (Show)
+
+
+firstName :: Person -> String
+firstName (Person firstname _ _ _ _ _) = firstname
+
+lastName :: Person -> String
+lastName (Person _ lastname  _ _ _ _) = lastname
+
+age :: Person -> Int
+age (Person _ _ age _ _ _) = age
+
+height :: Person -> Float
+height (Person _ _ _ height _ _) = height
+
+phoneNumber :: Person -> String
+phoneNumber (Person _ _ _ _ number _) = number 
+
+flavor :: Person -> String
+flavor (Person _ _ _ _ _ flavor) = flavor
+
+-- record syntax
+data Person2 = Person2 { firstName2 :: String
+                       , lastName2 :: String
+                       , age2 :: Int
+                       , height2 :: Float
+                       , phoneNumber2 :: String
+                       , flavor2 :: String
+                       } deriving (Show)
+
+-- type constructor with record syntax
+
+--- prefer this one
+data Car = Car { company :: String
+                , model :: String 
+                , year :: Int
+                } deriving (Show, Eq, Read)
+
+-- instead of this
+-- data Car a b c = Car { company :: a  
+--                      , model :: b  
+--                      , year :: c   
+--                      } deriving (Show) 
+
+tellCar :: Car -> String
+tellCar (Car {company = c, model = m, year = y}) = "This " ++ c ++ " " ++ m ++ " was made in " ++ show y 
+
+-- tellCar :: (Show a) => Car String String a -> String  
+-- tellCar (Car {company = c, model = m, year = y}) = "This " ++ c ++ " " ++ m ++ " was made in " ++ show y 
+
+data Vector a = Vector a a a deriving (Show)
+
+vplus :: (Num t) => Vector t -> Vector t -> Vector t
+(Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)
+
+vectMult :: (Num t) => Vector t -> t -> Vector t 
+(Vector i j k) `vectMult` m = Vector (i*m) (j*m) (k*m)
+
+scalarMult :: (Num t) => Vector t -> Vector t -> t  
+(Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n 
+
+-- derived instances
+
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
+           deriving (Eq, Ord, Show, Read, Bounded, Enum) 
