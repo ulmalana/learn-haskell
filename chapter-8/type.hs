46022b655ad9f86cdd798d48a328d7dc190cc8e7
ulmalana <muhamaul@gmail.com>
2021-12-17 20:49:00 +0700

    [Haskell] added typeclasses 102

--- chapter-8/type.hs
+++ chapter-8/type.hs
@@ -172,3 +172,29 @@ treeElem x (Node a left right)
  | x == a = True
  | x < a = treeElem x left
  | x > a = treeElem x right
+
+-- typeclasses
+
+-- class Eq a where  
+--     (==) :: a -> a -> Bool  
+--     (/=) :: a -> a -> Bool  
+--     x == y = not (x /= y)  
+--     x /= y = not (x == y)  
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+    Red == Red = True
+    Green == Green = True
+    Yellow == Yellow = True
+    _ == _ = False
+
+instance Show TrafficLight where
+    show Red = "Red Light"
+    show Yellow = "Yellow Light"
+    show Green = "Green Light"
+
+-- instance (Eq m) => Eq (Maybe m) where
+--     Just x == Just y = x == y 
+--     Nothing == Nothing = True
+--     _ == _ = False
