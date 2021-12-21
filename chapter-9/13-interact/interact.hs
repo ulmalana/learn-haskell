7415a5937862d4e6c48e0397175505db6883651d
ulmalana <muhamaul@gmail.com>
2021-12-21 21:02:25 +0700

    [Haskell] added part of chapter 9

---
+++ chapter-9/13-interact/interact.hs
@@ -0,0 +1,10 @@
+-- can be shortened to only
+-- main = interact $ unlines . filter ((<10) . length) . lines
+main = interact shortLinesOnly
+
+shortLinesOnly :: String -> String
+shortLinesOnly input = 
+    let allLines = lines input
+        shortLines = filter (\line -> length line < 10) allLines
+        result = unlines shortLines
+    in result
