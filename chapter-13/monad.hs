c82babdfd75234a2064eafce73b12a6e517e465f
Riz Maulana <muhamaul@gmail.com>
2022-01-07 18:52:23 +0700

    [Haskell] list as monad

--- chapter-13/monad.hs
+++ chapter-13/monad.hs
@@ -88,3 +88,62 @@ wopwop :: Maybe Char
 wopwop = do 
     (x:xs) <- Just ""
     return x 
+
+
+-- list as monad
+
+listOfTuples :: [(Int, Char)]
+listOfTuples = do 
+    n <- [1,2]
+    ch <- ['a', 'b']
+    return (n, ch)
+
+
+class Monad m => MonadPlus m where
+    mzero :: m a 
+    mplus :: m a -> m a -> m a 
+
+instance MonadPlus [] where
+    mzero = []
+    mplus = (++)
+
+guard :: (MonadPlus m) => Bool -> m ()
+guard True = return ()
+guard False = mzero
+
+sevensOnly :: [Int]
+sevensOnly = do 
+    x <- [1..50]
+    guard ('7' `elem` show x)
+    return x 
+
+
+-- posisi kuda catur
+
+type KnightPos = (Int, Int)
+
+moveKnight :: KnightPos -> [KnightPos]
+moveKnight (c,r) = do 
+    (c', r') <- [(c+2,r-1), (c+2, r+1), (c-2, r-1), (c-2, r+1)
+                ,(c+1, r-2), (c+1, r+2), (c-1, r-2), (c-1, r+2)
+                ]
+    guard (c' `elem` [1..8] && r' `elem` [1..8])
+    return (c', r')
+
+-- without monad
+moveKnight2 :: KnightPos -> [KnightPos]
+moveKnight2 (c,r) = filter onBoard 
+    [(c+2,r-1),(c+2,r+1),(c-2,r-1),(c-2,r+1)  
+    ,(c+1,r-2),(c+1,r+2),(c-1,r-2),(c-1,r+2) 
+    ]  
+    where onBoard (c,r) = c `elem` [1..8] && r `elem` [1..8] 
+
+
+in3 :: KnightPos -> [KnightPos]
+in3 start = do 
+    first <- moveKnight start
+    second <- moveKnight first
+    moveKnight second
+
+canReachIn3 :: KnightPos -> KnightPos -> Bool
+canReachIn3 start end = end `elem` in3 start
