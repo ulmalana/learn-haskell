f3f71868e3e91813c77661023c79330ecbecf4e7
Riz Maulana <muhamaul@gmail.com>
2022-02-03 20:32:03 +0700

    [Haskell] finished chapter 14

---
+++ RWH/chapter-14/simplestate.hs
@@ -0,0 +1,22 @@
+type SimpleState s a = s -> (a, s)
+type StringState a = SimpleState String a
+
+returnSt :: a -> SimpleState s a 
+returnSt a = \s -> (a, s)
+
+bindSt :: (SimpleState s a) -> (a -> SimpleState s b) -> SimpleState s b
+bindSt m k = \s -> let (a, s') = m s 
+                   in (k a) s'
+
+bindAlt :: (s -> (a, s))
+        -> (a -> s -> (b, s))
+        -> (s -> (b, s))
+bindAlt step makeStep oldState =
+    let (result, newState) = step oldState
+    in  (makeStep result) newState                   
+
+getSt :: SimpleState s s 
+getSt = \s -> (s, s)
+
+putSt :: s -> SimpleState s ()
+putSt s = \_ -> ((), s)    
