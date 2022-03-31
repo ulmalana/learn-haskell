a614ee236e6ab440b8e7df4e7464a0942f414fef
ulmalana <muhamaul@gmail.com>
2022-03-31 20:12:44 +0700

    [PIH] finished chapter 6

---
+++ PIH/ch6/Ch6.hs
@@ -0,0 +1,16 @@
+insert :: Ord a => a -> [a] -> [a]
+insert x []                 = [x]
+insert x (y:ys) | x <= y    = x : y : ys
+                | otherwise = y : insert x ys
+
+isort :: Ord a => [a] -> [a]
+isort []        = []
+isort (x:xs)    = insert x (isort xs)
+
+evens :: [a] -> [a]
+evens [] = []
+evens (x:xs) = x : odds xs
+
+odds :: [a] -> [a]
+odds [] = []
+odds (_:xs) = evens xs
