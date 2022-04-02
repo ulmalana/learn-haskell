97ccd9eaa608160406930f145a1d789ec339791a
ulmalana <muhamaul@gmail.com>
2022-04-02 14:12:32 +0700

    [PIH] finished chapter 8

--- PIH/ch8/Ch8.hs
+++ PIH/ch8/Ch8.hs
@@ -2,10 +2,38 @@ type Pos = (Int, Int)
 type Trans = Pos -> Pos
 type Pair a = (a,a)
 type Assoc k v = [(k,v)]
+type Subst = Assoc Char Bool
+type Bit = Int
+type Cont = [Op]
 
 data Move = North | South | East | West
 data Shape = Circle Float | Rect Float Float
 data Nat = Zero | Succ Nat
+data List a = Nil | Cons a (List a)
+data Tree a = Leaf a | Node (Tree a) a (Tree a)
+data Expr = Val Int | Add Expr Expr
+data Op = EVAL Expr | ADD Int
+data Prop = Const Bool
+          | Var Char
+          | Not Prop
+          | And Prop Prop
+          | Imply Prop Prop
+
+t :: Tree Int
+t = Node (Node (Leaf 1) 3 (Leaf 4)) 5
+         (Node (Leaf 6) 7 (Leaf 9))
+
+p1 :: Prop
+p1 = And (Var 'A') (Not (Var 'A'))
+
+p2 :: Prop
+p2 = Imply (And (Var 'A') (Var 'B')) (Var 'A')
+
+p3 :: Prop
+p3 = Imply (Var 'A') (And (Var 'A') (Var 'B'))
+
+p4 :: Prop
+p4 = Imply (And (Var 'A') (Imply (Var 'A') (Var 'B'))) (Var 'B')
 
 find :: Eq k => k -> Assoc k v -> v
 find k t = head [v | (k', v) <- t, k == k']
@@ -51,3 +79,76 @@ int2nat n = Succ (int2nat (n-1))
 
 add :: Nat -> Nat -> Nat
 add m n = int2nat (nat2int m + nat2int n)
+
+len :: List a -> Int
+len Nil = 0
+len (Cons _ xs) = 1 + len xs
+
+occurs :: Eq a => a -> Tree a -> Bool
+occurs x (Leaf y) = x == y
+occurs x (Node l y r) = x == y || occurs x l || occurs x r
+
+flatten :: Tree a -> [a]
+flatten (Leaf x) = [x]
+flatten (Node l x r) = flatten l ++ [x] ++ flatten r
+
+-- tautology checker
+
+eval :: Subst -> Prop -> Bool
+eval _ (Const b) = b
+eval s (Var x) = find x s
+eval s (Not p) = not (eval s p)
+eval s (And p q) = eval s p && eval s q
+eval s (Imply p q) = eval s p <= eval s q
+
+vars :: Prop -> [Char]
+vars (Const _) = []
+vars (Var x) = [x]
+vars (Not p) = vars p
+vars (And p q) = vars p ++ vars q
+vars (Imply p q) = vars p ++ vars q
+
+int2bin :: Int -> [Bit]
+int2bin 0 = []
+int2bin n = n `mod` 2 : int2bin (n `div` 2)
+
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs) = x : filter (/= x) (rmdups xs)
+
+bools :: Int -> [[Bool]]
+bools n = map (reverse . map conv . make n . int2bin) range
+            where
+                range = [0..(2^n)-1]
+                make n bs = take n (bs ++ repeat 0)
+                conv 0 = False
+                conv 1 = True
+
+bools' :: Int -> [[Bool]]
+bools' 0 = [[]]
+bools' n = map (False:) bss ++ map (True:) bss
+            where bss = bools' (n-1)
+
+substs :: Prop -> [Subst]
+substs p = map (zip vs) (bools (length vs))
+            where vs = rmdups (vars p)
+
+isTaut :: Prop -> Bool
+isTaut p = and [eval s p | s <- substs p]
+
+-- abstract machine
+value :: Expr -> Int
+value (Val n) = n
+value (Add x y) = value x + value y
+
+eval' :: Expr -> Cont -> Int
+eval' (Val n) c = exec c n
+eval' (Add x y) c = eval' x (EVAL y : c)
+
+exec :: Cont -> Int -> Int
+exec [] n = n
+exec (EVAL y : c) n = eval' y (ADD n : c)
+exec (ADD n : c) m = exec c (n+m)
+
+value' :: Expr -> Int
+value' e = eval' e [] 
