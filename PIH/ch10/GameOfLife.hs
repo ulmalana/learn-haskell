86ba141433fb04911e6ef9cbb192d5c4b3f2aafd
ulmalana <muhamaul@gmail.com>
2022-04-12 19:40:29 +0700

    [PIH] finished chapter 10

---
+++ PIH/ch10/GameOfLife.hs
@@ -0,0 +1,71 @@
+type Pos = (Int, Int)
+type Board = [Pos]
+
+width :: Int
+width = 10
+
+height :: Int
+height = 10
+
+cls :: IO ()
+cls = putStr "\ESC[2J"
+
+writeat :: Pos -> String -> IO ()
+writeat p xs = do
+                goto p
+                putStr xs
+
+goto :: Pos -> IO ()
+goto (x,y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+
+glider :: Board
+glider = [(4,2), (2,3), (3,4), (4,4)]
+
+showcells :: Board -> IO ()
+showcells b = sequence_ [writeat p "O" | p <- b]
+
+isAlive :: Board -> Pos -> Bool
+isAlive b p = elem p b
+
+isEmpty :: Board -> Pos -> Bool
+isEmpty b p = not (isAlive b p)
+
+neighbs :: Pos -> [Pos]
+neighbs (x,y) = map wrap [(x-1, y-1), (x, y-1),
+                          (x+1, y-1), (x-1, y),
+                          (x+1, y), (x-1, y+1),
+                          (x, y+1), (x+1, y+1)]
+
+wrap :: Pos -> Pos
+wrap (x,y) = (((x-1) `mod` width) + 1,
+              ((y-1) `mod` height) + 1)
+
+liveneighbs :: Board -> Pos -> Int
+liveneighbs b = length . filter (isAlive b) . neighbs
+
+survivors :: Board -> [Pos]
+survivors b = [p | p <- b, elem (liveneighbs b p) [2,3]]
+
+--births :: Board -> [Pos]
+--births b = [(x,y) | x <- [1..width], y <- [1..height], isEmpty b (x,y), liveneighbs b (x,y) == 3]
+-- OR
+births :: Board -> [Pos]
+births b = [p | p <- rmdups (concat (map neighbs b)), isEmpty b p, liveneighbs
+           b p == 3]
+
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs) =  x : rmdups (filter (/= x) xs)
+
+nextgen :: Board -> Board
+nextgen b = survivors b ++ births b
+
+wait :: Int -> IO ()
+wait n = sequence_ [return () | _ <- [1..n]]
+
+life :: Board -> IO ()
+life b = do
+            cls
+            showcells b
+            wait 500000
+            life (nextgen b)
