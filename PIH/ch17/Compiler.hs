5429489bdf3e3b3b96b155463595d877ef621c56
rz <muhamaul@gmail.com>
2022-05-18 13:31:11 +0700

    [Haskell] Programming in Haskell - FINISHED

--- PIH/ch17/Compiler.hs
+++ PIH/ch17/Compiler.hs
@@ -47,3 +47,53 @@ eval'' (Add x y) c s = eval'' x (eval'' y (c . add)) s
 
 eval3 :: Expr -> Cont
 eval3 e s = eval'' e id s
+
+-- 3 of 3 transformation steps
+-- defunctionalisation
+--
+-- in eval' and eval'' there are three continuation
+-- 1. halt the evaluation
+-- 2. push to the stack
+-- 3. add two top numbers on the stack
+--
+-- define three combinators for the above continuation
+haltC :: Cont
+haltC = id
+
+pushC :: Int -> Cont -> Cont
+pushC n c = c . push n
+
+addC :: Cont -> Cont
+addC c = c . add
+
+-- with above combinators, eval' and eval'' can be rewritten as
+-- eval' :: Expr -> Cont
+-- eval' e = eval '' e haltC
+--
+-- eval'' :: Expr -> Cont -> Cont
+-- eval'' (Val n) c = pushC n c
+-- eval'' (Add x y) c = eval'' x (eval'' y (addC c))
+
+-- new type for representing the combinators
+data Code = HALT | PUSH Int Code | ADD Code deriving Show
+
+-- this function maps combinators to new type Code
+-- exec :: Code -> Cont
+-- exec HALT = haltC
+-- exec (PUSH n c) = pushC n (exec c)
+-- exec (ADD c) = addC (exec c)
+
+-- exec is equivalent to:
+-- 
+exec :: Code -> Stack -> Stack
+exec HALT s = s
+exec (PUSH n c) s = exec c (n : s)
+exec (ADD c) (m : n : s) = exec c (n+m : s)
+
+-- this function compiles expression to code
+comp :: Expr -> Code
+comp e = comp' e HALT
+
+comp' :: Expr -> Code -> Code
+comp' (Val n) c = PUSH n c
+comp' (Add x y) c = comp' x (comp' y (ADD c))
