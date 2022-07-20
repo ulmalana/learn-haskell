c04fe0df6bf7e467d4d5686818b180356a1fa0ed
rz <muhamaul@gmail.com>
2022-07-20 14:21:09 +0700

    [HPFFP] updated chapter 25

--- HPFFP/ch25-composing-types/code.hs
+++ HPFFP/ch25-composing-types/code.hs
@@ -26,6 +26,11 @@ instance (Functor f, Functor g, Functor h) =>
     fmap f (Three fgha) = 
         Three $ (fmap . fmap . fmap) f fgha
 
+instance Applicative Identity where
+    pure = Identity
+
+    (Identity f) <*> (Identity a) = Identity (f a)
+
 instance (Applicative f, Applicative g) =>
             Applicative (Compose f g) where
     pure :: a -> Compose f g a
@@ -33,3 +38,27 @@ instance (Applicative f, Applicative g) =>
 
     (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
     (Compose fgf) <*> (Compose fga) = Compose $ (<*>) <$> fgf <*> fga
+
+-- Identity monad transformer
+newtype IdentityT f a =
+    IdentityT { runIdentityT :: f a } deriving (Eq, Show)
+
+instance (Functor m) => Functor (IdentityT m) where
+    fmap f (IdentityT fa) = IdentityT (fmap f fa)
+
+instance (Applicative m) => Applicative (IdentityT m) where
+    pure x = IdentityT (pure x)
+
+    (IdentityT fab) <*> (IdentityT fa) =
+        IdentityT (fab <*> fa)
+
+-- Compare Monad instance for Identity with Monad instance for IdentityT
+instance Monad Identity where
+    return = pure
+
+    (Identity a) >>= f = f a
+
+instance (Monad m) => Monad (IdentityT m) where
+    return = pure
+    (IdentityT ma) >>= f =
+        IdentityT $ ma >>= runIdentityT . f
